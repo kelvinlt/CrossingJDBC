6eb95d02daa39292a01cd0f1fca6dac55a8da314
kelvinlt <kelvinleethen@gmail.com>
2018-01-30 10:21:15 +0100

    1.Registrar un usuario. -Hecho 2.Obtener un usuario a partir de su nombre de usuario. -Hecho 3.Registrar un personaje. -Hecho

--- src/crossingjdbc/CrossingJDBC.java
+++ src/crossingjdbc/CrossingJDBC.java
@@ -1,16 +1,64 @@
 package crossingjdbc;
 import dao.CrossingDao;
+import java.util.ArrayList;
+import java.util.List;
+import obj.User;
+import obj.Character;
 
 public class CrossingJDBC {
 
     public static void main(String[] args) {
-        System.out.println("Conectado con la base datos...");
         CrossingDao crossingDao = new CrossingDao();
+        List<User> users = new ArrayList<>();
         try {
             //iniciar conexion con base de datos
             crossingDao.conectar();
+            
+            User kelvin= new User("kelvin", "123", 0, 0, "Barcelona", 0);
+            System.out.println("Insertando usuario "+kelvin.getUsername()+"...");
+            if(crossingDao.checkUser(kelvin.getUsername()) == false){
+                crossingDao.insertarUser(kelvin);
+            };
+            
+            User saiden= new User("saiden", "123", 0, 0, "Barcelona", 0);
+            System.out.println("Insertando usuario "+saiden.getUsername()+"...");
+            if(crossingDao.checkUser(saiden.getUsername()) == false){
+                crossingDao.insertarUser(saiden);
+            };
+
+            crossingDao.selectOneUser("kelvin"); 
+            
+            Character nameless = new Character("emiya","daw","tejado","flecha");
+            System.out.println("Insertando personaje " + nameless.getName()+ " a la base datos...");
+            if(crossingDao.checkCharacter(nameless.getName()) == false){
+                crossingDao.insertarCharacter(nameless);
+            };
+            
+            users = crossingDao.selectAllUser();
+            System.out.println("Lista de usuarios en la base de datos:");
+            for(User user : users) {
+                System.out.println("Username: "+user.getUsername());
+            }
+            System.out.println("----------------------------------");
+            
+            //cerrar conexion con base de datos
+            crossingDao.desconectar();
+            
+            
+            
         } catch (Exception e) {
         }
+    }
+    
+    public static void menu(){
+        System.out.println("------------------------------------");
+        System.out.println("1-Registrar nuevo usario");
+        System.out.println("2-Buscar usuario apartir de un nombre");
+        System.out.println("3-Registrar nuevo personaje");
+        System.out.println("4-Registrar nuevo item");
+        System.out.println("5-Login de usuario");
+        System.out.println("");
+        System.out.println("------------------------------------");
     }
     
 }
--- src/dao/CrossingDao.java
+++ src/dao/CrossingDao.java
@@ -24,17 +24,20 @@ public class CrossingDao {
         String user = "root";
         String pass = "";
         conexion = DriverManager.getConnection(url, user, pass);
+        System.out.println("Conectado con la base datos...");
+        System.out.println("----------------------------------");
     }
 
     public void desconectar() throws SQLException {
         if (conexion != null) {
             conexion.close();
+            System.out.println("Desconectado de la base datos...");
         }
     }
-    
+
     public void insertarUser(User u) throws SQLException {
         String insert = "insert into user values(?,?,?,?,?,?)";
-        PreparedStatement ps=conexion.prepareStatement(insert);
+        PreparedStatement ps = conexion.prepareStatement(insert);
         ps.setString(1, u.getUsername());
         ps.setString(2, u.getPassword());
         ps.setInt(3, 100);
@@ -43,67 +46,136 @@ public class CrossingDao {
         ps.setInt(6, 0);
         ps.executeUpdate();
         ps.close();
+        System.out.println("----------------------------------");
     }
-    
+
     public List<User> selectAllUser() throws SQLException {
         List<User> users = new ArrayList<>();
-        String select= "select * from user";
-        Statement st=conexion.createStatement();
-        ResultSet rs=st.executeQuery(select);
-        while(rs.next()){
-         User u= new User();
-         users.add(u);
+        String select = "select * from user";
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(select);
+        while (rs.next()) {
+            User u = new User();
+            u.setUsername(rs.getString("username"));
+            users.add(u);
         }
         return users;
     }
-    
+
+    public void selectOneUser(String searchName) throws SQLException {
+        User userReturn = new User();
+        
+        String select = "select * from user where username='"  + searchName+"'";
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(select);
+
+        System.out.println("Buscando usuario: "+searchName);
+        while (rs.next()) {
+            System.out.println("Username: "+rs.getString("username"));
+            System.out.println("Password: "+rs.getString("password"));
+            System.out.println("Level: "+rs.getInt("level"));
+            System.out.println("Points: "+rs.getInt("points"));
+            System.out.println("Stucoins: "+rs.getInt("stucoins"));
+            System.out.println("Place: "+rs.getString("place"));
+            System.out.println("----------------------------------");
+        }
+    }
+
+    public boolean checkUser(String username) throws SQLException {
+        boolean check = false;
+        List<User> users = new ArrayList<>();
+        String select = "select * from user";
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(select);
+        while (rs.next()) {
+            User u = new User();
+            u.setUsername(rs.getString("username"));
+            if (username.equals(u.getUsername())) {
+                check = true;
+                System.out.println("-El usuario " + username + " ya existe");
+                System.out.println("----------------------------------");
+            }
+        }
+        return check;
+    }
+
     public List<Character> selectAllCharacter() throws SQLException {
         List<Character> characters = new ArrayList<>();
-        String select="select * from character";
-        Statement st=conexion.createStatement();
-        ResultSet rs=st.executeQuery(select);
-        while(rs.next()){
-            Character c=new Character();
+        String select = "select * from character";
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(select);
+        while (rs.next()) {
+            Character c = new Character();
             characters.add(c);
         }
         return characters;
     }
     
+        public void insertarCharacter(Character c) throws SQLException {
+        String insert = "insert into stucomcrossing.character values(?,?,?,?)";
+        PreparedStatement ps = conexion.prepareStatement(insert);
+        ps.setString(1, c.getName());
+        ps.setString(2, c.getStudy());
+        ps.setString(3, c.getPlace());
+        ps.setString(4, c.getPreference());
+        ps.executeUpdate();
+        ps.close();
+        System.out.println("----------------------------------");
+    }
+        
+    public boolean checkCharacter(String name) throws SQLException {
+        boolean check = false;
+        List<Character> characters = new ArrayList<>();
+        String select = "select * from stucomcrossing.character";
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(select);
+        while (rs.next()) {
+            Character c = new Character();
+            c.setName(rs.getString("name"));
+            if (name.equals(c.getName())) {
+                check = true;
+                System.out.println("-El personaje " + name + " ya existe");
+                System.out.println("----------------------------------");
+            }
+        }
+        return check;
+    }    
+    
+
     public List<Item> selectAllItem() throws SQLException {
         List<Item> items = new ArrayList<>();
-        String select="select * from item";
-        Statement st=conexion.createStatement();
-        ResultSet rs=st.executeQuery(select);
-        while(rs.next()){
-            Item i=new Item();
+        String select = "select * from item";
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(select);
+        while (rs.next()) {
+            Item i = new Item();
             items.add(i);
         }
         return items;
     }
-    
+
     public List<Contact> selectAllContact() throws SQLException {
         List<Contact> contacts = new ArrayList<>();
-        String select="select * from contact";
-        Statement st=conexion.createStatement();
-        ResultSet rs=st.executeQuery(select);
-        while(rs.next()){
-            Contact c=new Contact();
+        String select = "select * from contact";
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(select);
+        while (rs.next()) {
+            Contact c = new Contact();
             contacts.add(c);
         }
         return contacts;
     }
-    
+
     public List<Inventory> selectAllInventory() throws SQLException {
         List<Inventory> inventories = new ArrayList<>();
-        String select="select * from inventory";
-        Statement st=conexion.createStatement();
-        ResultSet rs=st.executeQuery(select);
-        while(rs.next()){
-            Inventory i=new Inventory();
+        String select = "select * from inventory";
+        Statement st = conexion.createStatement();
+        ResultSet rs = st.executeQuery(select);
+        while (rs.next()) {
+            Inventory i = new Inventory();
             inventories.add(i);
         }
         return inventories;
     }
-    
-    
+
 }
